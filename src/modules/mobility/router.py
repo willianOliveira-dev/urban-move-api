2588662126537025795090563c25a59dd7afdfbd
Willian Oliveira <willian.dev.tech@gmail.com>
2026-04-22 15:15:07 -0300

    feat: implement authentication on routes

--- src/modules/mobility/router.py
+++ src/modules/mobility/router.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from src.modules.auth.dependencies import get_current_user_id
 from src.modules.mobility.routes.router import router as routes_router
 from src.modules.mobility.search.router import router as search_router
 from src.modules.mobility.sptrans.router import router as sptrans_router
@@ -7,7 +7,10 @@ from src.modules.mobility.stops.router import router as stops_router
 from src.modules.mobility.trips.router import router as trips_router
 from src.modules.mobility.vehicles.router import router as vehicles_router
 
-router = APIRouter(prefix="/api/v1/mobility")
+router = APIRouter(
+    prefix="/api/v1/mobility",
+    dependencies=[Depends(get_current_user_id)],
+)
 
 router.include_router(routes_router)
 router.include_router(stops_router)
